cbbc3e56f4941fb774adc9a3ff67db0f5cd64db7
DarrenVs <DarrenVanstaen@Gmail.com>
2016-02-15 14:22:57 +0100

    Weapon Progress update 1

--- Assets/Scripts/Humanoid.cs
+++ Assets/Scripts/Humanoid.cs
@@ -22,13 +22,15 @@ public class Humanoid : MonoBehaviour
     protected bool characterCanFloat = false;
 
 
+    //Function to use the selected weapon
     protected void useTool()
     {
-        if (weaponList.Count > 0)
+        if (weaponList.Count > 0 && weaponList[selectedWeapon % weaponList.Count])
             weaponList[selectedWeapon % weaponList.Count].attack();
     }
 
 
+    //Function to move the character around in a direction (the speed will be calculated depending on the Vector3 'moveDirection'). moveDirection's range goes from -1 to +1 and will be multiplied by the moveSpeed value.
     protected void move(Vector3 moveDirection)
     {
         
--- Assets/Scripts/WeaponScript.cs
+++ Assets/Scripts/WeaponScript.cs
@@ -3,12 +3,40 @@ using System.Collections;
 
 public class WeaponScript : MonoBehaviour {
 
+    [SerializeField]
+    float maxAttackCooldown = 4;
+    float attackCooldown = 0;
+
     float attackDamage;
+    bool attacking = false;
+    Animator animator;
+    
+
+    void Start()
+    {
+        animator = transform.GetComponent<Animator>();
+    }
 
     public void attack()
     {
-        foreach (Collider col in Physics.OverlapSphere(transform.position + transform.forward * 2, 1)) Debug.Log(col.transform.name);
+        if (attackCooldown <= 0)
+        {
+            animator.SetBool("Attacking", true);
+            attackCooldown = maxAttackCooldown;
+        }
+    }
 
+	void Update()
+	{
         
+        if (attackCooldown <= 0 && animator.GetBool("Attacking"))
+            animator.SetBool("Attacking", false);
+
+        attackCooldown -= Time.deltaTime;
+	}
+
+    void OnTriggerEnter(Collider Col)
+    {
+        Debug.Log(Col.transform.name);
     }
 }
