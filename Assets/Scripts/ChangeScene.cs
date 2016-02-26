1afd43626f42bd03b2c4d4f731723e866692a606
jimbo333 <19198@ma-web.nl>
2016-02-26 15:45:09 +0100

    GraphicsMenu FullyFunctional

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+	public void ChangeToScene (int sceneToChangeTo){
+		SceneManager.LoadScene (sceneToChangeTo);
+	}
+}
