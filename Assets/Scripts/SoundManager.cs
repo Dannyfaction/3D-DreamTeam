40f014b6f030193d6dc405216236719fcb8b58b3
jimbo333 <19198@ma-web.nl>
2016-03-07 16:59:20 +0100

    Sound Option Added

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+	public float SoundSetting;
+	public Toggle SoundToggle;
+		
+	// Update is called once per frame
+	void Update () {
+		SoundSlider (SoundSetting);
+	}
+
+	public void SoundSlider(float volume) {
+		AudioListener.volume = volume;
+
+		SoundSetting = volume;
+
+		//GetComponent<AudioSource> ().outputAudioMixerGroup;
+	}
+
+}
