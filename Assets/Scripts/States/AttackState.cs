2637c2a4228932fbe70b21a97c75982d59e6137d
Tim <tbajmat@gmail.com>
2016-03-14 08:41:46 +0100

    Small Delete

--- Assets/Scripts/States/AttackState.cs
+++ Assets/Scripts/States/AttackState.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class AttackState : State {
 
 	private float distanceToTarget;
-
 	public int sightDistance;
 	Vector3 aiPos;
 	Vector3 playerPos;
@@ -12,22 +11,12 @@ public class AttackState : State {
 	public override void Act()
 	{
         useTool();
-		/*	
-		 * 	Dealing damgage value 5-10?
-		 * 	Health system van de player koppellen aan AttackState
-		 * 	Proto bevat: Attack, Chase en wander; uiteindelijk: Flee..
-		 * 	De hoop word minder met de dag dat ik wacht..
-		 * 
-		 *
-		 */
 
 		playerPos = targetGetter().transform.position;
 		aiPos =  transform.position - playerPos;
 		transform.position = playerPos + Vector3.Normalize(aiPos) * 5;
 	}
 
-
-	
 	public override void Reason(){
 		distanceToTarget = Vector3.Distance(targetGetter().transform.position, transform.position);
 		if(distanceToTarget > 5 )
