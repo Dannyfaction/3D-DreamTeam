8d2e31b9611a1bf6970a62a08a7370e13bfabdff
SkaterDanny <dannykruiswijk@hotmail.com>
2016-03-14 09:23:12 +0100

    Imported New Placeholder (Ethan) Character

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : Character {
+
+	Vector3 inputDelta = new Vector3();
+	Transform camera;
+
+	// Use this for initialization
+	void Start () {
+		camera = Camera.main.transform.parent.transform;
+		base.Start ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		// Read the Inputs
+		inputDelta = new Vector3(Input.GetAxis( "Horizontal" ), 0, Input.GetAxisRaw( "Vertical" ));
+
+
+		// Give the commands
+		Move (transform.position - camera.TransformDirection(inputDelta * -1));
+
+
+		// Fire Fixed update
+		base.FixedUpdate ();
+	}
+}
